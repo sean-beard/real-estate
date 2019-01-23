f95080bfaa7cd7bb6dd79b0ab46ac981ff3670cb
Sean Beard <sean61793@my.uri.edu>
2019-01-22 21:18:26 -0500

    ui(input): style input element and text

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 
-import { colors } from "./typography";
+import { Breakpoints, colors } from "./typography";
 
 export const Input = styled.input`
   width: 150px;
-  height: 20px;
+  height: 2rem;
+  border-radius: 3px;
+  font-size: 1rem;
+  color: ${colors.font};
+  box-sizing: border-box;
+
+  @media (${Breakpoints.mobile}) {
+    width: 95%%;
+  }
+
+  &:hover {
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.15);
+  }
 `;
 
 export const InlineText = styled.span`
